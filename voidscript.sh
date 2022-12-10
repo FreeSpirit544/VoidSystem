cf8876425b01f4810d60bb3ebbd9ed3a7d869c4e
FreeSpirit544 <102427278+FreeSpirit544@users.noreply.github.com>
2022-12-10 19:32:33 +0100

    Update voidscript.sh

--- voidscript.sh
+++ voidscript.sh
@@ -2,6 +2,7 @@
 xbps-install -Su
 xbps-install harfbuzz-devel base-devel libX11-devel libXft-devel libXinerama-devel nerd-fonts font-awesome xorg i3-gaps bumblebee-status firefox-esr picom feh mpv ranger neofetch vim pfetch pavucontrol pulseaudio 
 cd ..
+mkdir .config/i3
 cp voidscript/config .config/i3/config
 cd voidscript
 mkdir /etc/X11/xorg.conf.d
