35eb1b9e6338f74b8ce1088142fa774bfa01d7fe
FreeSpirit544 <102427278+FreeSpirit544@users.noreply.github.com>
2022-12-10 19:05:52 +0100

    Add files via upload

---
+++ voidscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+xbps-install -Su
+xbps-install harfbuzz-devel base-devel libX11-devel libXft-devel libXinerama-devel nerd-fonts font-awesome xorg i3-gaps bumblebee-status firefox-esr picom feh mpv ranger neofetch vim pfetch pavucontrol pulseaudio 
+cd ..
+cp voidscript/config .config/i3/config
+cd voidscript
+mkdir /etc/X11/xorg.conf.d
+cp 20-intel.conf /etc/X11/xorg.conf.d/20-intel.conf
+cd st
+make clean install
+cd ..
+cd dmenu 
+make clean install
+cd ..
+cd ..
+cp voidscript/xinitrc .xinitrc
+cp voidscript/vimrc .vimrc
+mkdir Wallpapers
+cp voidscript/Wallpaper.jpg Wallpapers/Wallpaper.jpg
